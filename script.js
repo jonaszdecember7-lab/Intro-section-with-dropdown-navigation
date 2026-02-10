db229cce9f8b3498a48f0506c7b5dc1f8394816e
Jonasz <jonaszgrudzien1234@gmail.com>
2026-02-10 17:50:57 +0000

    Basic functionality of mobile menu done

--- script.js
+++ script.js
@@ -6,4 +6,24 @@ dropdownButtons.forEach(button => {
         menu.classList.toggle('active');
         button.classList.toggle('open');
     });
-}); 
+});
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const pageContent = document.querySelector('main');
+const openMenu = document.querySelector('.open-menu');
+const closeMenu = document.querySelector('.close-menu');
+const mainContent = document.querySelector('main');
+
+openMenu.addEventListener('click', toggleMenu);
+closeMenu.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    mobileMenu.toggleAttribute('hidden');
+    mainContent.classList.toggle('darkerScreen');
+    pageContent.toggleAttribute('inert', hidden);
+    document.body.classList.toggle('menu-open');
+
+    const isOpen = !mobileMenu.hasAttribute('hidden');
+    openMenu.setAttribute('aria-expanded', isOpen);
+    closeMenu.setAttribute('aria-expanded', isOpen);
+}
